04247b8aba500c6a301da97a7bcaa8cd6862ada7
root <root@stage.sitewatch>
2019-11-18 14:35:13 +0000

    test deploy

--- deploy-scripts/deploy-10uplabs.sh
+++ deploy-scripts/deploy-10uplabs.sh
@@ -3,4 +3,4 @@
 # Catch Errors
 set -euo pipefail
 
-rsync  -vrxc --delete --dry-run ./ gitlab@10uplabs.com:/var/www/html/wordpress/publishermediakit/wp-content/plugins/  --exclude-from=$CI_PROJECT_DIR/deploy-scripts/rsync-excludes-10uplabs.txt
+rsync  -vrxc --delete --dry-run ./ gitlab@10uplabs.com:/var/www/html/wordpress/publishermediakit/wp-content/plugins/publisher-media-kit/  --exclude-from=$CI_PROJECT_DIR/deploy-scripts/rsync-excludes-10uplabs.txt
