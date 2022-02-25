049c583057497819a6e7735f6c16d74da622dccd
Faisal Alvi <faisalalvi33@gmail.com>
2022-02-25 21:20:45 +0530

    fixing cypress test

--- tests/cypress/integration/pmk-block-patterns.test.js
+++ tests/cypress/integration/pmk-block-patterns.test.js
@@ -6,20 +6,15 @@ describe( 'Check if Media Kit Block Pattern is available for use', () => {
         cy.get( '.edit-post-header-toolbar__inserter-toggle' ).click();
         cy.get( '.components-tab-panel__tabs button:nth-child(2)' ).click();
 
-
-
         //(add version) If dropdown is available. (After WP 5.?)
-
-
         cy.get( 'body' ).then( ( $body ) => {
             if ( $body.find('.components-select-control__input' ).length > 0 ) {
                 cy.get( '.components-select-control__input' ).select('publisher-media-kit', { force: true });
             }
         });
 
-
-        cy.get( '[aria-label="Publisher Media Kit - Cover"]' ).click({ force: true });
-        //cy.get('.edit-post-header-toolbar__inserter-toggle').click();
+        cy.get( '[aria-label="Publisher Media Kit - Cover"] .block-editor-block-preview__container' ).click({ force: true });
+        cy.get('.edit-post-header-toolbar__inserter-toggle').click();
         cy.get( '[data-type="core/heading"]' ).should( 'contain.text', 'Media Kit' );
     } );
 } );
