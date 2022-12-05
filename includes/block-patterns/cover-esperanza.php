d936d55538a820db0a71ff86300e09909ff46060
faisal-alvi <faisalalvi33@gmail.com>
2022-12-05 22:23:49 +0530

    cover block support for wp 5.7

--- includes/block-patterns/cover-esperanza.php
+++ includes/block-patterns/cover-esperanza.php
@@ -1,18 +1,18 @@
 <?php
 // phpcs:ignoreFile
 ?>
-<!-- wp:cover {"url":"https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg","id":18,"dimRatio":50,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":430,"minHeightUnit":"px","isDark":false,"align":"full"} -->
-<div class="wp-block-cover alignfull is-light" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-18" alt="" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"white","className":"has-background-color","fontSize":"x-large"} -->
-        <h1 class="has-text-align-center has-background-color has-white-color has-text-color has-x-large-font-size" style="font-style:normal;font-weight:400">Media Kit</h1>
-        <!-- /wp:heading -->
+<!-- wp:cover {"url":"https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg","id":18,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":430,"minHeightUnit":"px","align":"full","className":"is-light"} -->
+<div class="wp-block-cover alignfull has-background-dim is-light" style="min-height:430px"><img class="wp-block-cover__image-background wp-image-18" alt="" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"white","className":"has-background-color","fontSize":"x-large"} -->
+<h1 class="has-text-align-center has-background-color has-white-color has-text-color has-x-large-font-size" style="font-style:normal;font-weight:400">Media Kit</h1>
+<!-- /wp:heading -->
 
-        <!-- wp:paragraph {"align":"center","textColor":"white","fontSize":"small"} -->
-        <p class="has-text-align-center has-background-color has-text-color has-small-font-size">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.</p>
-        <!-- /wp:paragraph -->
+<!-- wp:paragraph {"align":"center","textColor":"white","fontSize":"small"} -->
+<p class="has-text-align-center has-white-color has-text-color has-small-font-size">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.</p>
+<!-- /wp:paragraph -->
 
-        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-        <div class="wp-block-buttons"><!-- wp:button {"fontSize":"medium"} -->
-            <div class="wp-block-button has-custom-font-size has-medium-font-size"><a class="wp-block-button__link">CONTACT US</a></div>
-            <!-- /wp:button --></div>
-        <!-- /wp:buttons --></div></div>
+<!-- wp:buttons {"contentJustification":"center"} -->
+<div class="wp-block-buttons is-content-justification-center"><!-- wp:button {"className":"has-custom-font-size has-medium-font-size"} -->
+<div class="wp-block-button has-custom-font-size has-medium-font-size"><a class="wp-block-button__link">CONTACT US</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
 <!-- /wp:cover -->
