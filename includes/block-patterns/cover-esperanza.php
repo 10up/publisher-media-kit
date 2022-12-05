e8f7fffa7c23d06825a34c6aebb7c20e23f1b428
faisal-alvi <faisalalvi33@gmail.com>
2022-12-05 18:43:44 +0530

    color fix in storefront for cover block

--- includes/block-patterns/cover-esperanza.php
+++ includes/block-patterns/cover-esperanza.php
@@ -2,11 +2,11 @@
 // phpcs:ignoreFile
 ?>
 <!-- wp:cover {"url":"https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg","id":18,"dimRatio":50,"focalPoint":{"x":"0.50","y":"0.50"},"minHeight":430,"minHeightUnit":"px","isDark":false,"align":"full"} -->
-<div class="wp-block-cover alignfull is-light" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-18" alt="" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"background","fontSize":"x-large"} -->
-        <h1 class="has-text-align-center has-background-color has-text-color has-x-large-font-size" style="font-style:normal;font-weight:400">Media Kit</h1>
+<div class="wp-block-cover alignfull is-light" style="min-height:430px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-18" alt="" src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Becharof_National_Wildlife_Refuge%2CAlaska.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"textColor":"white","className":"has-background-color","fontSize":"x-large"} -->
+        <h1 class="has-text-align-center has-background-color has-white-color has-text-color has-x-large-font-size" style="font-style:normal;font-weight:400">Media Kit</h1>
         <!-- /wp:heading -->
 
-        <!-- wp:paragraph {"align":"center","textColor":"background","fontSize":"small"} -->
+        <!-- wp:paragraph {"align":"center","textColor":"white","fontSize":"small"} -->
         <p class="has-text-align-center has-background-color has-text-color has-small-font-size">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.</p>
         <!-- /wp:paragraph -->
 
