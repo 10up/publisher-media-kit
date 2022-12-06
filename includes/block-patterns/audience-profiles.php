b8ed29684e08961f8d3a6b74805c3c578d82cdb9
faisal-alvi <faisalalvi33@gmail.com>
2022-12-06 17:33:28 +0530

    fix audience profile patter for wp 5.8

--- includes/block-patterns/audience-profiles.php
+++ includes/block-patterns/audience-profiles.php
@@ -3,32 +3,32 @@
 ?>
 <!-- wp:group -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"top":"4.38rem"}}},"fontSize":"large"} -->
-    <h2 class="has-text-align-center has-large-font-size" style="font-style:normal;font-weight:400;margin-top:4.38rem">Audience Profiles</h2>
-    <!-- /wp:heading -->
+<h2 class="has-text-align-center has-large-font-size" style="font-style:normal;font-weight:400;margin-top:4.38rem">Audience Profiles</h2>
+<!-- /wp:heading -->
 
-    <!-- wp:separator {"style":{"color":{"background":"#ebf0f7"}},"className":"is-style-wide"} -->
-    <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#ebf0f7;color:#ebf0f7"/>
-    <!-- /wp:separator -->
+<!-- wp:separator {"className":"has-text-color has-alpha-channel-opacity has-background is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide"/>
+<!-- /wp:separator -->
 
-    <!-- wp:columns -->
-    <div class="wp-block-columns"><!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image {"align":"center","id":21,"width":74,"height":160,"sizeSlug":"full","linkDestination":"none"} -->
-            <figure class="wp-block-image aligncenter size-full is-resized"><img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Woman_-_The_Noun_Project.svg" alt="" class="wp-image-21" width="74" height="160"/></figure>
-            <!-- /wp:image -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":21,"width":74,"height":160,"linkDestination":"none","className":"size-full"} -->
+<div class="wp-block-image size-full"><figure class="aligncenter is-resized"><img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Woman_-_The_Noun_Project.svg" alt="" class="wp-image-21" width="74" height="160"/></figure></div>
+<!-- /wp:image -->
 
-            <!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-            <p class="has-text-align-center has-medium-font-size">The average female visitor is<br><strong>38 years old</strong></p>
-            <!-- /wp:paragraph --></div>
-        <!-- /wp:column -->
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">The average female visitor is<br><strong>38 years old</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image {"align":"center","id":20,"width":59,"height":160,"sizeSlug":"full","linkDestination":"none"} -->
-            <figure class="wp-block-image aligncenter size-full is-resized"><img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Man_-_The_Noun_Project.svg" alt="" class="wp-image-20" width="59" height="160"/></figure>
-            <!-- /wp:image -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":20,"width":59,"height":160,"linkDestination":"none","className":"size-full"} -->
+<div class="wp-block-image size-full"><figure class="aligncenter is-resized"><img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Man_-_The_Noun_Project.svg" alt="" class="wp-image-20" width="59" height="160"/></figure></div>
+<!-- /wp:image -->
 
-            <!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-            <p class="has-text-align-center has-medium-font-size">The average male visitor is<br><strong>52 years old</strong></p>
-            <!-- /wp:paragraph --></div>
-        <!-- /wp:column --></div>
-    <!-- /wp:columns --></div>
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">The average male visitor is<br><strong>52 years old</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
 <!-- /wp:group -->
