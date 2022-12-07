040ccc0832412ed9848581905c8e8b4d2c21236b
JK <jayedulk@gmail.com>
2022-12-07 23:07:23 +0600

    Fix tab spacing and modal closer

--- tests/cypress/integration/pmk-block-patterns.test.js
+++ tests/cypress/integration/pmk-block-patterns.test.js
@@ -1,30 +1,24 @@
 describe('Check if Media Kit Block Pattern is available for use', () => {
-	it('Can insert the block pattern', () => {
-		cy.visitAdminPage('post-new.php');
-		cy.wait(5000);
-		cy.get('body').then((body) => {
-			if (body.find('button[aria-label="Close dialog"]').length > 0) {
-				cy.get('button[aria-label="Close dialog"]').click();
-			}
-		});
-		cy.wait(5000);
-		cy.get('#post-title-0, h1.editor-post-title__input').click().type('Test Block Pattern');
-		cy.get('.edit-post-header-toolbar__inserter-toggle').click();
-		cy.get('.components-tab-panel__tabs button:nth-child(2)').click();
+    it('Can insert the block pattern', () => {
+        cy.visitAdminPage('post-new.php');
+        cy.closeWelcomeGuide();
+        cy.get('#post-title-0, h1.editor-post-title__input').click().type('Test Block Pattern');
+        cy.get('.edit-post-header-toolbar__inserter-toggle').click();
+        cy.get('.components-tab-panel__tabs button:nth-child(2)').click();
 
-		// (add version) If dropdown is available. (After WP 5.?)
-		cy.get('body').then(($body) => {
-			if ($body.find('.components-select-control__input').length > 0) {
-				cy.get('.components-select-control__input').select('publisher-media-kit', {
-					force: true,
-				});
-			}
-		});
+        // (add version) If dropdown is available. (After WP 5.?)
+        cy.get('body').then(($body) => {
+            if ($body.find('.components-select-control__input').length > 0) {
+                cy.get('.components-select-control__input').select('publisher-media-kit', {
+                    force: true,
+                });
+            }
+        });
 
-		cy.get(
-			'[aria-label="Publisher Media Kit - Cover"] .block-editor-block-patterns-list__item-title',
-		).click({ force: true });
-		cy.get('.edit-post-header-toolbar__inserter-toggle').click();
-		cy.get('[data-type="core/heading"]').should('contain.text', 'Media Kit');
-	});
+        cy.get(
+            '[aria-label="Publisher Media Kit - Cover"] .block-editor-block-patterns-list__item-title',
+        ).click({ force: true });
+        cy.get('.edit-post-header-toolbar__inserter-toggle').click();
+        cy.get('[data-type="core/heading"]').should('contain.text', 'Media Kit');
+    });
 });
