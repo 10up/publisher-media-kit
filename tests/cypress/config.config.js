dd1924c9f42b8819230ed2618b795459466de615
JK <jayedulk@gmail.com>
2022-12-07 16:13:39 +0600

    Code optimization

--- tests/cypress/config.config.js
+++ tests/cypress/config.config.js
@@ -13,11 +13,7 @@ module.exports = defineConfig({
 		setupNodeEvents(on, config) {
 			return require(`${__dirname}/plugins/index.js`)(on, config);
 		},
-		specPattern: [
-			`${__dirname}/integration/pmk-plugin-activation.test.js`,
-			`${__dirname}/integration/pmk-media-kit-page-creation.test.js`,
-			`${__dirname}/integration/pmk-block-patterns.test.js`,
-		],
+		specPattern: `${__dirname}/integration/*.test.{js,jsx,ts,tsx}`,
 		supportFile: `${__dirname}/support/index.js`,
 	},
 });
