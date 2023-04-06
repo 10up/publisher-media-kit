b8d3cdde20e6e3e6614d1ca850fe20d95e907224
JK <jayedulk@gmail.com>
2023-04-06 15:22:35 +0600

    Cypress test update

--- tests/cypress/integration/pmk-block-patterns.test.js
+++ tests/cypress/integration/pmk-block-patterns.test.js
@@ -12,12 +12,13 @@ describe('Check if Media Kit Block Pattern is available for use', () => {
                 cy.get('.components-select-control__input').select('publisher-media-kit', {
                     force: true,
                 });
-            }
+            } else if ( $body.find( '[aria-label="Publisher Media Kit"]' ).length > 0 ) {
+				cy.get('[aria-label="Publisher Media Kit"]').click();
+			}
         });
 
-        cy.get(
-            '[aria-label="Publisher Media Kit - Cover"] .block-editor-block-patterns-list__item-title',
-        ).click({ force: true });
+        cy.get('[aria-label="Publisher Media Kit - Cover"]').click();
+		
         cy.get('.edit-post-header-toolbar__inserter-toggle').click();
         cy.get('[data-type="core/heading"]').should('contain.text', 'Media Kit');
     });
