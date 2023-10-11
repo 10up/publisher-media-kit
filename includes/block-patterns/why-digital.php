9eebbffceb682046e9030abe74f6ba82f63ab340
faisal-alvi <faisalalvi33@gmail.com>
2023-10-11 18:52:32 +0530

    update all patterns as per the latest WP

--- includes/block-patterns/why-digital.php
+++ includes/block-patterns/why-digital.php
@@ -1,4 +1,58 @@
 <?php
 // phpcs:ignoreFile
 ?>
-<!-- wp:group {"align":"wide","className":"pmk-why-digital"} --><div class="wp-block-group alignwide pmk-why-digital"><!-- wp:heading {"level":2} --><h2 class="has-text-align-center">Why you should choose digital</h2><!-- /wp:heading --><!-- wp:columns {"align":"wide"} --><div class="wp-block-columns alignwide"><!-- wp:column {"className":"pmk-digital-column pmk-digital-column-source"} --><div class="wp-block-column pmk-digital-column pmk-digital-column-source"><!-- wp:image {"align":"left","width":35,"height":35} --><figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-1.png" alt="Female visitors" width="35" height="35"/></figure><!-- /wp:image --><!-- wp:heading {"level":3} --><h3>Source</h3><!-- /wp:heading --><!-- wp:paragraph --><p>10up is the leading news source in Sacramento. Making it Northern California’s GO-TO choice to stay informed on anything news, weather, sports &amp; entertainment.</p><!-- /wp:paragraph --></div><!-- /wp:column --><!-- wp:column {"className":"pmk-digital-column pmk-digital-column-visibility"} --><div class="wp-block-column pmk-digital-column pmk-digital-column-visibility"><!-- wp:image {"align":"left","width":35,"height":35} --><figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-2.png" alt="Female visitors" width="35" height="35"/></figure><!-- /wp:image --><!-- wp:heading {"level":3} --><h3>Visibility</h3><!-- /wp:heading --><!-- wp:paragraph --><p>All ad units across the site have a viewability percentage of 70% of higher, making “above-the-fold” a thing of the past!</p><!-- /wp:paragraph --></div><!-- /wp:column --><!-- wp:column {"className":"pmk-digital-column pmk-digital-column-measure"} --><div class="wp-block-column pmk-digital-column pmk-digital-column-measure"><!-- wp:image {"align":"left","width":35,"height":35} --><figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-3.png" alt="Female visitors" width="35" height="35"/></figure><!-- /wp:image --><!-- wp:heading {"level":3} --><h3>Measure</h3><!-- /wp:heading --><!-- wp:paragraph --><p>10up has Measurable ROI! Ads on 10up.com have high click-through percentages against industry benchmark rates.</p><!-- /wp:paragraph --></div><!-- /wp:column --></div><!-- /wp:columns --><!-- wp:buttons {"contentJustification":"center"} --><div class="wp-block-buttons is-content-justification-center"><!-- wp:button {"className":"is-style-fill pmk-button"} --><div class="wp-block-button is-style-fill pmk-button"><a class="wp-block-button__link">CONTACT US</a></div><!-- /wp:button --></div><!-- /wp:buttons --></div><!-- /wp:group -->
+<!-- wp:group {"align":"wide","className":"pmk-why-digital"} -->
+<div class="wp-block-group alignwide pmk-why-digital"><!-- wp:heading {"className":"has-text-align-center"} -->
+<h2 class="wp-block-heading has-text-align-center">Why you should choose digital</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"className":"pmk-digital-column pmk-digital-column-source"} -->
+<div class="wp-block-column pmk-digital-column pmk-digital-column-source"><!-- wp:image {"align":"left","width":35,"height":35} -->
+<figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-1.png" alt="Female visitors" style="width:35px;height:35px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Source</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>10up is the leading news source in Sacramento. Making it Northern California’s GO-TO choice to stay informed on anything news, weather, sports &amp; entertainment.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"className":"pmk-digital-column pmk-digital-column-visibility"} -->
+<div class="wp-block-column pmk-digital-column pmk-digital-column-visibility"><!-- wp:image {"align":"left","width":35,"height":35} -->
+<figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-2.png" alt="Female visitors" style="width:35px;height:35px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Visibility</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>All ad units across the site have a viewability percentage of 70% of higher, making “above-the-fold” a thing of the past!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"className":"pmk-digital-column pmk-digital-column-measure"} -->
+<div class="wp-block-column pmk-digital-column pmk-digital-column-measure"><!-- wp:image {"align":"left","width":35,"height":35} -->
+<figure class="wp-block-image alignleft is-resized"><img src="<?php echo esc_url( PUBLISHER_MEDIA_KIT_URL ); ?>assets/images/icon-digital-3.png" alt="Female visitors" style="width:35px;height:35px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Measure</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>10up has Measurable ROI! Ads on 10up.com have high click-through percentages against industry benchmark rates.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill pmk-button"} -->
+<div class="wp-block-button is-style-fill pmk-button"><a class="wp-block-button__link wp-element-button">CONTACT US</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
