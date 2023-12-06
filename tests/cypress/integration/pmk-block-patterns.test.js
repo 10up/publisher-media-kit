4c7b5201ce688913bc3a48d573fe43e5b395eaf5
faisal-alvi <faisalalvi33@gmail.com>
2023-12-06 17:56:13 +0530

    update the test

--- tests/cypress/integration/pmk-block-patterns.test.js
+++ tests/cypress/integration/pmk-block-patterns.test.js
@@ -15,10 +15,9 @@ describe('Check if Media Kit Block Pattern is available for use', () => {
             } else if ($body.find( '[aria-label="Publisher Media Kit"]' ).length > 0) {
                 cy.get('[aria-label="Publisher Media Kit"]').click();
             }
-        });
 
-        cy.get('[aria-label="Publisher Media Kit - Cover"]').click();
-        cy.get('.edit-post-header-toolbar__inserter-toggle').click();
-        cy.get('[data-type="core/heading"]').should('contain.text', 'Media Kit');
+            // Check if cover patter exist in the list
+            cy.get('[aria-label="Publisher Media Kit - Cover"]').should('exist');
+        });
     });
 });
